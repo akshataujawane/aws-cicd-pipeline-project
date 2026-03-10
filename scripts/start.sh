a842703205685a73bb99aebb3112a95f25a0d219
Akshata Ujawane <akshataujawane19@gmail.com>
2026-03-10 13:04:36 +0530

    fix deployment pipeline

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo "Restarting Apache"
+echo "Starting deployment..."
+
+sudo apt update -y
+sudo apt install apache2 -y
 
 sudo systemctl restart apache2
+
+echo "Deployment successful"
